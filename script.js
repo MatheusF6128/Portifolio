decce3b442a5e56fa88bbfafed86ea52c16295bb
matheus freitas <matheus_6161_@outlook.com>
2022-10-17 18:13:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//efeito maquina de escrever
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 120 * i);
+    });
+}
+const titulo = document.getElementById('machine');
+typeWriter(titulo);
+
+
+//funcionalidade de toggle do menu
+let show = true; 
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
